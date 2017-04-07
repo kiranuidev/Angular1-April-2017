297463a1672167004bf64f82ea780c76cce9aaf3
kiranpaturi <kiran_p@k3informatics.com>
2017-04-07 09:23:55 +0530

    Addedcode

--- example2.js
+++ example2.js
@@ -1,9 +1,11 @@
 function getFullName(){
-    var firstName = document.getElementById("txtFirstName");
-    var lastName =document.getElementById("txtLastName");
-    var fullName = document.getElementById("txtFullName");
-    var result= firstName.value+lastName.value;
+    var firstNameCtrl = document.getElementById("txtFirstName");
+    var lastNameCtrl =document.getElementById("txtLastName");
+    var fullNameCtrl = document.getElementById("txtFullName");
+    
+    var result= firstNameCtrl.value+lastNameCtrl.value;
     console.log(result);
+    
     //this willl write the data back to html full name text box.....
-    fullName.value=result;
+    fullNameCtrl.value=result;
 }
